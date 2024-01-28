aae22a0fe0eb311c613f84d58d22a2435b11736c
Serg Vergun <1197314+severgun@users.noreply.github.com>
2024-01-28 23:25:10 +0300

    feat: add create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { ifError } from "assert";
+import fs from "fs/promises";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const FILE_NAME = "fresh.txt";
+  const FILE_CONTENT = "I am fresh and young";
+  const DIR_PATH = "./files";
+  const FILE_PATH = path.join(DIR_PATH, FILE_NAME);
+
+  try {
+    await fs.stat(FILE_PATH);
+    const err = new Error();
+    err.message = `FS operation failed`;
+    err.code = "EXISTS";
+    throw err;
+  } catch (error) {
+    if (error.code === "EXISTS") {
+      throw error;
+    }
+
+    const fd = await fs.open(FILE_PATH, "w");
+    await fd.writeFile(FILE_CONTENT);
+    fd.close();
+  }
 };
 
 await create();
