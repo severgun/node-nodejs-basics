f0855c57c01863d4f2f6efca4c5f58e2b57c75a9
Serg Vergun <1197314+severgun@users.noreply.github.com>
2024-01-29 21:39:06 +0300

    feat: add child process spawn solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import child_process from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  //const child = child_process.fork("./files/script.js", args);
+
+  const child = await child_process.spawn("node", [
+    "./files/script.js",
+    ...(args || []),
+  ]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
