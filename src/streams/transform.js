ef1d72ca4b18f811ca09ad13144e1d4352ba1d8f
Sergey Vergun <sewergun@gmail.com>
2024-01-29 15:54:05 +0300

    feat: add transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const data = chunk.toString().split("").reverse().join("");
+
+      callback(null, data + "\n");
+    },
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
