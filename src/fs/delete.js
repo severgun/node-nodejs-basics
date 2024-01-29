b662f717fbe76c05eb3a015b8e3ef42ff8113c14
Sergey Vergun <sewergun@gmail.com>
2024-01-29 12:15:45 +0300

    feat: add delete solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+
 const remove = async () => {
-    // Write your code here 
+  const FILE_PATH = "./files/fileToRemove.txt";
+
+  fs.access(FILE_PATH, fs.constants.F_OK, async (err) => {
+    if (err) throw new Error("FS operation failed");
+
+    await fs.rm(FILE_PATH, (err) => {
+      if (err) throw err;
+    });
+  });
 };
 
 await remove();
