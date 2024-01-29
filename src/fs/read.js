761af9c6e5d779b96bbc2f1ffac15e7a3a888ecc
Sergey Vergun <sewergun@gmail.com>
2024-01-29 12:33:33 +0300

    feat: add read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const FILE_PATH = "./files/fileToRead.txt";
+
+  await fs.access(FILE_PATH, fs.constants.F_OK, async (err) => {
+    if (err && err.code === "ENOENT") {
+      throw Error("FS operation failed");
+    }
+    if (err) throw err;
+
+    await fs.readFile(FILE_PATH, {}, (err, data) => {
+      if (err) throw err;
+
+      console.log(data.toString());
+    });
+  });
 };
 
 await read();
