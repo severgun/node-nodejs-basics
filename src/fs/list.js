03bc35438f20ca1c74a59209193eee8efedcdb73
Sergey Vergun <sewergun@gmail.com>
2024-01-29 12:27:46 +0300

    feat: add list solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+  const DIR_PATH = "./files";
+
+  await fs.stat(DIR_PATH, async (err, stats) => {
+    if ((err && err.code === "ENOENT") || !stats.isDirectory()) {
+      throw Error("FS operation failed");
+    }
+
+    await fs.readdir(DIR_PATH, { recursive: true }, (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        console.log(file);
+      }
+    });
+  });
 };
 
 await list();
