1c6462d2f1ce9f80fcebb15fa842fa6c7bf5803c
Sergey Vergun <sewergun@gmail.com>
2024-01-29 14:00:07 +0300

    feat: add args solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+
+  const parsedArgs = [];
+  for (let index = 0; index < args.length; index++) {
+    const element = args[index];
+    if (element.startsWith("--")) {
+      parsedArgs.push(`${element.slice(2)} is ${args[index + 1]}`);
+      index++;
+    }
+  }
+
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
