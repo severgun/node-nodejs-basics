153ca497bd0538016e38c016e146025d702bce9c
Sergey Vergun <sewergun@gmail.com>
2024-01-29 10:33:25 +0300

    fix: rewrite with callback api

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,28 +1,23 @@
-import { ifError } from "assert";
-import fs from "fs/promises";
+import fs from "fs";
 import path from "path";
 
 const create = async () => {
+  const DIR_PATH = "./files";
   const FILE_NAME = "fresh.txt";
   const FILE_CONTENT = "I am fresh and young";
-  const DIR_PATH = "./files";
   const FILE_PATH = path.join(DIR_PATH, FILE_NAME);
 
-  try {
-    await fs.stat(FILE_PATH);
-    const err = new Error();
-    err.message = `FS operation failed`;
-    err.code = "EXISTS";
-    throw err;
-  } catch (error) {
-    if (error.code === "EXISTS") {
-      throw error;
+  await fs.access(FILE_PATH, fs.constants.F_OK, async (err) => {
+    if (!err) {
+      throw Error("FS operation failed");
     }
 
-    const fd = await fs.open(FILE_PATH, "w");
-    await fd.writeFile(FILE_CONTENT);
-    fd.close();
-  }
+    await fs.writeFile(FILE_PATH, FILE_CONTENT, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
 };
 
 await create();
